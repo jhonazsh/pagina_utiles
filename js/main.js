b3550a7602f8c2619489cea7684ca18c2e3c7924
jhon medina <jhofe_s17@hotmail.com>
2014-01-28 18:21:05 -0500

    cambios en navegacion vertical izquierda

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 $(".list_hija").eq(0).show();
 
-
 $("p").eq(0).on("click", {id:0}, mostrar);
 $("p").eq(1).on("click", {id:1}, mostrar);
 $("p").eq(2).on("click", {id:2}, mostrar);
-$("p").eq(3).on("click", {id:3}, mostrar);
 
 
 function mostrar(event){
 	if(event.data.id==0){
-		$(".list_hija").eq(0).toggle(500);
+		$(".list_hija").eq(0).toggle(250);
+		$(".list_hija").eq(1).hide(250);
+		$(".list_hija").eq(2).hide(250);
 	}
 
 	if(event.data.id==1){
-		$(".list_hija").eq(1).toggle(500);
+		$(".list_hija").eq(1).toggle(250);
+		$(".list_hija").eq(0).hide(250);
+		$(".list_hija").eq(2).hide(250);
 	}
 
 	if(event.data.id==2){
-		$(".list_hija").eq(2).toggle(500);
-	}
-
-	if(event.data.id==3){
-		$(".list_hija").eq(3).toggle(500);
+		$(".list_hija").eq(2).toggle(250);
+		$(".list_hija").eq(1).hide(250);
+		$(".list_hija").eq(0).hide(250);
 	}
 };
 
@@ -29,4 +29,32 @@ function mostrar(event){
 $('.carousel').carousel({
 	interval:3000
 });
+
+$("p").eq(0).on("mouseover", {id:0}, mostraricono);
+$("p").eq(1).on("mouseover", {id:1}, mostraricono);
+$("p").eq(2).on("mouseover", {id:2}, mostraricono);
+
+function mostraricono(event){
+	if(event.data.id==0){
+		$(".sh").eq(0).show(250);
+		$(".sh").eq(1).hide(250);
+		$(".sh").eq(2).hide(250);
+	}
+
+	if(event.data.id==1){
+		$(".sh").eq(1).show(250);
+		$(".sh").eq(2).hide(250);
+		$(".sh").eq(0).hide(250);
+	}
+
+	if(event.data.id==2){
+		$(".sh").eq(2).show(250);
+		$(".sh").eq(1).hide(250);
+		$(".sh").eq(0).hide(250);
+	}
+}
+
+
+
+
 
